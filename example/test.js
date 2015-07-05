bcc260e6a07815b5acf4e6234eb673cf8125b6d7
James Talmage <james@talmage.io>
2015-07-05 01:30:50 -0400

    drop node 8 support

--- example/test.js
+++ example/test.js
@@ -1,7 +1,6 @@
 var https = require('https');
 var certs = require('../');
 var assert = require('assert');
-var node8 = require('semver').lt(process.version, '0.9.0');
 
 var server = https.createServer(certs.server, function(req, res){
   res.write('hello world');
@@ -10,24 +9,33 @@ var server = https.createServer(certs.server, function(req, res){
 
 
 function client() {
-  https.get({
+  var options = {
     host: 'localhost',
     port: 37124,
     path: '/',
-    agent: node8 ? false : new https.Agent({ca: [certs.ca.cert]})
-  }, function(res) {
-    var buf = '';
-    res.setEncoding('utf8');
-    res.on('data', function(data) {
-      buf += data;
-    });
-    res.on('end', function() {
-      assert.equal(buf, 'hello world');
-      console.log('TEST PASSED');
-      process.exit(0);
-    });
+    agent: new https.Agent({ca:[certs.ca.cert]})
+  };
 
-  }).on('error', assert.fail.bind(assert));
+  https.get(options, handleResponse).on('error', handleError);
+}
+
+function handleError (err) {
+  console.log(err);
+  process.exit(1);
+}
+
+function handleResponse (res) {
+  var buf = '';
+  res.setEncoding('utf8');
+  res.on('data', function(data) {
+    buf += data;
+  });
+  res.on('end', function() {
+    assert.equal(buf, 'hello world');
+    console.log('TEST PASSED');
+    process.exit(0);
+  });
+  res.on('error', handleError);
 }
 
 setTimeout(function() {
